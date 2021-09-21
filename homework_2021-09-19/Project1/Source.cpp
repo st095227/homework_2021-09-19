b9c80c7644cd6a6ea21f43818c006a895732b650
st095227 <antonov.semen2015@gmail.com>
2021-09-21 21:08:46 +0300

    homework_2021-09-19

--- homework_2021-09-19/Project1/Source.cpp
+++ homework_2021-09-19/Project1/Source.cpp
@@ -5,6 +5,6 @@ int main(int argc, char* argv[]) {
 	int a = 0;
 	cin >> a;
 	cout << "The next number for the number "<< a <<" is "<< a+1 <<"."<< endl;
-	cout << "The previous number for the number " << a << " is " << a - 1 << "."<< endl;
+	cout << "The previous number for the номер " << a << " is " << a - 1 << "."<< endl;
 	return EXIT_SUCCESS;
 }
--- homework_2021-09-19/Project10/Source10.cpp
+++ homework_2021-09-19/Project10/Source10.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
 	int a = 0;
 	int b = 0;
-	cin >> a >> b;
-	cout << (a + b) / 2 + abs(a - b) / 2;
+	int mod = 0;
+	int c = 0;
+
+	cin >> a;
+	cin >> b;
+	c = (a/b) * b + (b/a) * a;
+	mod = a % b + b % a;
+	mod = (mod % a) % b;
+	c += mod;
+	c = c / (c / (a + b) + 1);
+	cout << c << endl;
+
 	return EXIT_SUCCESS;
 }
