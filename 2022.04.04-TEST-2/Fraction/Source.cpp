51e8083a520af856e0839c9c54b9b9b198c6c6a9
st095227 <antonov.semen2015@gmail.com>
2022-04-04 13:03:15 +0300

    Test 2

---
+++ 2022.04.04-TEST-2/Fraction/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Fraction.h"
+
+using namespace std;
+
+int main()
+{
+	Fraction fr1(3, 1);
+	Fraction fr2(5, -2);
+	cout << fr1 * fr2 << endl;
+	cout << fr1 / fr2 << endl;
+	cout << 1 / (3 * fr1 + 4 * fr2) << endl;
+	cout << fr1.absF() << endl; //модуль числа
+	cout << fr1.reverse() << endl; //обратное число
+	cout << fr2.power(2) << endl;
+	if (3 == fr1)
+	{
+		cout << '1';
+	}
+	else
+	{
+		cout << "2";
+	}
+	Fraction f4(8, -32), f0 (16, -72);
+	cout << endl << f4 + f0 << endl;
+	return 0;
+}
